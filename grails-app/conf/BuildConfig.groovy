d5c7ca1ac72d22f340c75bbd344de60ab6ea0980
Eric Brandwein <eric.brandwein@mercadolibre.com>
2015-03-31 12:51:35 -0300

    Problemas con el httpbuilder

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -49,6 +49,7 @@ grails.project.dependency.resolution = {
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
         runtime 'mysql:mysql-connector-java:5.1.29'
+        compile('org.codehaus.groovy.modules.http-builder:http-builder:0.5.0')
         // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
         test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
     }
@@ -67,6 +68,7 @@ grails.project.dependency.resolution = {
         runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
+        runtime ":rest:0.7"
 
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
--- grails-app/controllers/lefrage/SearchController.groovy
+++ grails-app/controllers/lefrage/SearchController.groovy
@@ -5,16 +5,18 @@ import security.*
 import lefrage.User
 import searchs.SearchService
 import groovy.json.JsonSlurper
+import groovyx.net.http.*
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
 
 @Secured(['ROLE_USER'])
-class SearchController {
-
+class SearchController {    
     def springSecurityService
     def searchService
 
     def index() {
-    	if(params.optionValue=="Meli")redirect(action: "meli", params: params)
-    	else redirect(action: "friends", params: params)
+        if(params.optionValue=="Meli")redirect(action: "meli", params: params)
+        else redirect(action: "friends", params: params)
     }
 
     def friends(){
@@ -26,7 +28,22 @@ class SearchController {
     def meli(){
         def currentSpringUser = springSecurityService.currentUser
         def user = User.findBySpringUser(currentSpringUser)
-        def favourited = user.queries.find{it.queryString==params.item_search} != null
+        def query = user.queries.find{it.queryString==params.item_search}
+        def favourited = query != null
+        if (favourited){
+            def http = new HTTPBuilder('https://api.mercadolibre.com/sites/MLA/search')
+            //Obtain http builder class
+            def html = http.get( query : [q:'params.item_search'] ){ resp, reader ->
+                def prices = reader.results.price
+                def stat = new Stat(date:new Date())
+                    .calcMean(prices)
+                    .calcDeviation(prices)
+                    .calcMax(prices)
+                    .calcMin(prices)
+                query.addToStats(stat)
+                user.addToQueries(query)
+            }
+        }
         def postDestination = user.friends
         postDestination.add(user) //add myself to it
     	[search: params.item_search, optionName: params.optionName, 
